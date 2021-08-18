95b105857d4ea479fd961e51e4ac76385446ec7d
peekapeka <86009813+peekapeka@users.noreply.github.com>
2021-08-18 22:16:52 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST, PORT = "", 8080;
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1);
+s.bind((HOST,PORT));
+s.listen(True);
+
+print("Serving HTTP on port %s...." %PORT);
+
+while True:
+
+    client_connection, client_address = s.accept();
+    request = client_connection.recv(1024);
+    print(request.decode("utf-8"));
+    http_response = ""\
+
+HTTP/1.1 200 OK
+
+Content-Type: text/html; charset=UTF-8
+Welcome This is my first webpage, GREAT!
+""
+    client_connection.sendall(bytes(http_response, "utf-8"));
+    client_connection.close();
